4f539eb4e80200fff8c6c1caff5fc8ff01016f8b
Alexanne Brunold <alexanne.brunold@hetic.net>
2021-11-03 12:47:30 +0100

    ADD config SCSS and header for mobile

---
+++ components/BaseButton.vue
@@ -0,0 +1,50 @@
+<template>
+  <button
+    :class="buttonStyle"
+    :form="formId"
+    :disabled="showButtonLoading"
+    @click="$emit('button-click')"
+  >
+    <div v-if="showButtonLoading" class="loader" />
+    <slot v-else />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'BaseButton',
+  props: {
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+    formId: {
+      type: String,
+      default: '',
+    },
+    typeOfButton: {
+      type: String,
+      default: 'primaryButton',
+    },
+  },
+  computed: {
+    buttonStyle () {
+      switch (this.typeOfButton) {
+        case 'primaryButton':
+          return 'button-primary'
+          case 'primaryButtonWhite':
+            return 'button-primary-white'
+        case 'cancelButton':
+          return 'button-cancel'
+        case 'successButton':
+          return 'button-success'
+        default:
+          return 'button-primary'
+      }
+    },
+    showButtonLoading () {
+      return this.loading && this.typeOfButton !== 'cancelButton'
+    },
+  },
+}
+</script>
---
+++ pages_components/Navigation.vue
@@ -0,0 +1,12 @@
+<template>
+  <nav class="flex">
+    <h1>Do It Natural</h1>
+
+    <ul>
+      <li>Maison</li>
+      <li>Beauté</li>
+      <li>Articles</li>
+      <BaseButton typeOfButton="primaryButton"> Coucou </BaseButton>
+    </ul>
+  </nav>
+</template>
